69b0459c7bfae628210760e5d66e04de3483c8d7
jonathannicolas <jonathan.s.nicolas@gmail.com>
2017-08-28 18:07:38 +0200

    add all the backend

---
+++ functions/index.js
@@ -0,0 +1,144 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+const format = require('util').format;
+const Storage = require('@google-cloud/storage');
+
+
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+var DIR = './uploads/';
+
+app.use(bodyParser.json());
+
+
+//var upload = multer({dest: DIR}).single('photo');
+/* GET home page. */
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+    }
+});
+
+var upload = multer({ //multer settings
+    //storage: storage
+    storage: multer.memoryStorage()
+}).single('photo');
+
+app.get('/', function(req, res, next) {
+// render the index page, and pass data to it.
+    res.send('working');
+});
+
+//our file upload function.
+//router.post('/', function (req, res, next) {
+//    var path = '';
+//    upload(req, res, function (err) {
+//        if (err) {
+//            // An error occurred when uploading
+//            console.log(err);
+//            return res.status(422).send("an Error occured")
+//        }
+//        // No error occured.
+//        path = req.file.path;
+//        return res.send("Upload Completed for "+path);
+//    });
+//})
+
+
+
+/** API path that will uploads the files */
+
+app.post('/uploads', function(req, res) {
+    console.log("hello file", req);
+    console.log("yo");
+    const fs = require('fs');
+    const gcs = require('@google-cloud/storage')({
+        projectId: 'fileupload-177714',
+        keyFilename: './keyfile.json'
+    });
+
+    const bucket = gcs.bucket('doppleruploadfile');
+    upload(req,res,function(err){
+        console.log("hello file 2", req.file);
+
+        const blob = bucket.file(req.file.originalname);
+
+        const blobStream = blob.createWriteStream();
+
+        blobStream.on('error', (err) => {
+            next(err);
+        });
+
+        //blob
+        //    .makePublic()
+        //    .then(() => {
+        //        console.log('public');
+        //    })
+        //    .catch((err) => {
+        //        console.error('ERROR:', err);
+        //    });
+
+        blobStream.on('finish', () => {
+            // The public URL can be used to directly access the file via HTTP.
+            const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+            console.log(publicUrl);
+            blob
+                .makePublic()
+                .then(() => {
+                    console.log('public');
+                })
+                .then(() => {
+                    res.status(200).json({
+                        url: publicUrl
+                    });
+
+                })
+                .catch((err) => {
+                    console.error('ERROR:', err);
+                });
+
+            //res.status(200).json({
+            //    url: publicUrl
+            //});
+        });
+
+        blobStream.end(req.file.buffer);
+
+        //if(err){
+        //    res.json({error_code:1,err_desc:err});
+        //    return;
+        //}
+        //res.json({error_code:0,err_desc:null});
+    });
+
+});
+
+exports.api = functions.https.onRequest(app);
+
+//app.listen('3001', function(){
+//    console.log('running on 3001...');
+//});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
